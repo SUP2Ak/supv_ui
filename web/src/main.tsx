93e791cdb8a6ca57dee5d21f5888233ab018bb83
SUP2Ak <cormier.wesley@gmail.com>
2025-02-24 01:29:03 +0100

    ✨ feat(main): add FontAwesome icon library support
    - import FontAwesome core and icon sets for solid, regular, and brands
    - add icons to the library for use throughout the application

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import App from './App.tsx'
 import './index.scss'
+
+library.add(fas, far, fab);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
